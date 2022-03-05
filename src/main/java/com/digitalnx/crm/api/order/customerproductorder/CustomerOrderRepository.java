bc08bb7006776c1dc0a4fa3ec3f073794e26e71d
Iman Alavi Fazel <im.alavifazel@gmail.com>
2022-03-06 02:09:14 +0330

    Add a new route for Order controller

--- src/main/java/com/digitalnx/crm/api/order/customerproductorder/CustomerOrderRepository.java
+++ src/main/java/com/digitalnx/crm/api/order/customerproductorder/CustomerOrderRepository.java
@@ -1,6 +1,11 @@
 package com.digitalnx.crm.api.order.customerproductorder;
 
+import com.digitalnx.crm.api.user.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerOrderRepository extends CrudRepository<CustomerProductOrder, Integer> {
+    List<CustomerProductOrder> findByUser(Optional<User> user);
 }
